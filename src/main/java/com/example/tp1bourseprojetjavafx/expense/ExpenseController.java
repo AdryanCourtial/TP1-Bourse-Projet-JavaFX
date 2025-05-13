6fca34d284b630369cb30328af38c5eabb7899fc
AdryanCourtial <adryan.courtial@ynov.com>
2025-05-13 17:03:01 +0200

    :construction: Ajout du system de INSERT pour les Expense + Verification

--- src/main/java/com/example/tp1bourseprojetjavafx/expense/ExpenseController.java
+++ src/main/java/com/example/tp1bourseprojetjavafx/expense/ExpenseController.java
@@ -1,5 +1,42 @@
 package com.example.tp1bourseprojetjavafx.expense;
 
+import com.example.tp1bourseprojetjavafx.MainApplication;
+import com.example.tp1bourseprojetjavafx.database.queries.ExpenseDAO;
+import com.example.tp1bourseprojetjavafx.dialog.AddExpenseDialogue.AddExpenseController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import java.io.IOException;
+import java.util.Objects;
+
 public class ExpenseController {
 
+     @FXML
+    public void redirectToDialogExpense() throws IOException {
+         FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("dialog-add-expense.fxml"));
+         DialogPane dialogPane = loader.load();
+
+         AddExpenseController controller = loader.getController();
+
+         Dialog<Expense> dialog = new Dialog<>();
+         dialog.setTitle("Ajouter une dépense");
+         dialog.setDialogPane(dialogPane);
+
+         dialog.setResultConverter((buttonType) -> {
+             System.out.println("JE VIENS DE CLIQUER");
+            if (!Objects.equals(ButtonBar.ButtonData.OK_DONE, buttonType.getButtonData())) {
+                return null;
+            }
+
+            ExpenseDAO expenseDAO = new ExpenseDAO();
+            expenseDAO.insertExpense(controller.getExpense());
+            return controller.getExpense();
+
+         });
+
+         dialog.showAndWait();
+
+     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,10 @@ module com.example.tp1bourseprojetjavafx {
 
 
     opens com.example.tp1bourseprojetjavafx to javafx.fxml;
+    opens com.example.tp1bourseprojetjavafx.dialog.AddExpenseDialogue to javafx.fxml;
+    opens com.example.tp1bourseprojetjavafx.expense to javafx.fxml;
+
     exports com.example.tp1bourseprojetjavafx;
     exports com.example.tp1bourseprojetjavafx.expense;
+    exports com.example.tp1bourseprojetjavafx.dialog.AddExpenseDialogue;
 }
